ea872958d6fc7e4d0119326d4a3b5aedc54d3e9c
Chris Strange <mr.chris.strange@gmail.com>
2025-05-18 17:44:29 -0500

    Update dependencies & yarn constraints config

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -1,12 +1,24 @@
+// @ts-check
+
 const { defineConfig } = require('@yarnpkg/types')
 
 /**
- * @typedef {import('@yarnpkg/types').Yarn} Yarn
- * @typedef {import('@yarnpkg/types').Context} Config
- * @typedef {import('@yarnpkg/types').Context} Context
- * @typedef {import('@yarnpkg/types').Workspace} Workspace
- * @typedef {import('@yarnpkg/types').Dependency} Dependency
+ * @typedef {import('@yarnpkg/types').Yarn.Constraints.Yarn} Yarn
+ * @typedef {import('@yarnpkg/types').Yarn.Constraints.Context} Context
+ * @typedef {import('@yarnpkg/types').Yarn.Constraints.Workspace} Workspace
+ * @typedef {import('@yarnpkg/types').Yarn.Constraints.Dependency} Dependency
  */
+
+/**
+ * @typedef ProjectVersion
+ * @property {string} name the name of the project, with or without an npm scope
+ * @property {string} [version] the required version of the project, or undefined/unset to disable constraints on the project's version
+ */
+
+/**
+ * @type {ProjectVersion[]}
+ */
+const projectVersions = [{ name: '@the-stranger/eslint-plugin' }]
 
 /**
  * Require that any dependency of a project is also a dependency of the root workspace.
@@ -14,7 +26,7 @@ const { defineConfig } = require('@yarnpkg/types')
  * @param {Workspace} project a project in the workspace
  */
 async function enforceConsistentProjectDependencies(yarn, project) {
-  const rootWs = yarn.workspace({ cwd: '.' })
+  const rootWs = yarn.workspace({ cwd: '.' }) ?? undefined
 
   for (const pkg of yarn.dependencies({ workspace: project })) {
     if (pkg.type === 'peerDependencies') {
@@ -25,34 +37,47 @@ async function enforceConsistentProjectDependencies(yarn, project) {
     if (rootPkg) {
       pkg.update(rootPkg.range)
     } else {
-      pkg.delete()
+      pkg.error(`"${pkg.ident}" is not installed in the workspace.`)
     }
   }
 }
 
 /**
- * Define the engines.node field of a workspace's package.json.
+ * Define the engines.node field of a workspace's package.json. If the field is defined in the root manifest, the version specified therein is enforced. If the field isn't defined, no constraint is required.
  * @param {Workspace} project a project in the workspace
+ * @param {string|null} [version] the value specified in the workspace-level package.json
  */
-function setNodeEngine(project) {
-  project.set('engines.node', '^22')
+function setNodeEngine(project, version) {
+  if (version) {
+    project.set('engines.node', version)
+  }
 }
 
 /**
  * Synchronize a project's version with the workspace version.
  * @param {Workspace} project a project in the workspace
- * @param {string} wsVersion the version of the root package.json
+ * @param {string|null} [wsVersion] the version of the root package.json
  */
 function setProjectVersion(project, wsVersion) {
-  project.set('version', wsVersion)
+  const projectVersion = projectVersions.find(
+    e => project.ident && e.name.endsWith(project.ident),
+  )
+  if (projectVersion) {
+    if (projectVersion.version) {
+      project.set('version', projectVersion.version)
+    }
+  } else {
+    project.set('version', wsVersion)
+  }
 }
 
 module.exports = defineConfig({
   async constraints({ Yarn }) {
+    const root = Yarn.workspace({ cwd: '.' })
     for (const ws of Yarn.workspaces()) {
-      setNodeEngine(ws)
-      setProjectVersion(ws, Yarn.workspace({ cwd: '.' }).pkg.version)
       enforceConsistentProjectDependencies(Yarn, ws)
+      setNodeEngine(ws, root?.manifest?.['engines']?.['node'])
+      setProjectVersion(ws, root?.pkg.version)
     }
   },
 })
