3c52f4fe56db7331faec266fe47ef7b3eeaf5c18
Chris Strange <mr.chris.strange@gmail.com>
2025-06-27 11:05:51 -0500

    chore: don't tell me how long my commit lines can be

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,4 +2,7 @@ import type { UserConfig } from '@commitlint/types'
 
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [0],
+  },
 } satisfies UserConfig
